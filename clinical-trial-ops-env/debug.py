6da953d48a2e20b1682b8e645cc6d51dd269d9f1
Akhil Madesh.G <gudiseakhil21@gmail.com>
2026-04-08 15:31:32 +0530

    Delete clinical-trial-ops-env directory

--- clinical-trial-ops-env/debug.py
+++
@@ -1,9 +0,0 @@
-import traceback
-from env.environment import ClinicalTrialEnv
-
-try:
-    env = ClinicalTrialEnv(task_id='lab_dose_management')
-    env.reset()
-except Exception as e:
-    with open('err.txt', 'w', encoding='utf-8') as f:
-        traceback.print_exc(file=f)
